eb144500098840feef393048e392d169e1a15fd6
jinelei <jinelei@163.com>
2019-04-12 21:16:28 +0800

    add channel restore

--- src/main/java/cn/jinelei/rainbow/smart/server/handler/HeartbeatHandler.java
+++ src/main/java/cn/jinelei/rainbow/smart/server/handler/HeartbeatHandler.java
@@ -1,5 +1,6 @@
 package cn.jinelei.rainbow.smart.server.handler;
 
+import cn.jinelei.rainbow.smart.server.container.ConnectionContainer;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.ReferenceCountUtil;
@@ -23,21 +24,30 @@ public class HeartbeatHandler extends ChannelInboundHandlerAdapter {
                 && Message.Tag.HEARTBEAT.equals(((Message.Pkt) msg).getTag())) {
             Message.Pkt pkt = (Message.Pkt) msg;
             LOGGER.debug("{}: received: {}", ctx.channel().id(), msg);
-            Message.Pkt rsp = Message.Pkt.newBuilder()
-                    .setDstAddr(pkt.getSrcAddr())
-                    .setSrcAddr("0")
-                    .setDir(false)
-                    .setSeq(pkt.getSeq() + 1)
-                    .setTag(pkt.getTag())
-                    .setTimestamp(new Date().getTime())
-                    .setHeartbeatRspMsg(
-                            Message.HeartbeatRspMsg.newBuilder()
-                                    .setErrcode(Common.ErrCode.SUCCESS)
-                    ).build();
+            if (ConnectionContainer.getInstance().getOnlineMap().containsKey(ctx.channel().id())) {
+                ctx.writeAndFlush(getPktHeartbeatRsp(pkt));
+            } else if (ConnectionContainer.getInstance().getSuddenDeathMap().containsKey(ctx.channel().id())) {
+                ConnectionContainer.getInstance().suddenDeathToOnline(ctx.channel().id());
+                LOGGER.debug("{}: suddendeath to online", ctx.channel().id());
+                ctx.writeAndFlush(getPktHeartbeatRsp(pkt));
+            }
             ReferenceCountUtil.release(msg);
-            ctx.writeAndFlush(rsp);
         } else {
             ctx.fireChannelRead(msg);
         }
+    }
+
+    private Message.Pkt getPktHeartbeatRsp(Message.Pkt pkt) {
+        return Message.Pkt.newBuilder()
+                .setDstAddr(pkt.getSrcAddr())
+                .setSrcAddr("0")
+                .setDir(false)
+                .setSeq(pkt.getSeq() + 1)
+                .setTag(pkt.getTag())
+                .setTimestamp(new Date().getTime())
+                .setHeartbeatRspMsg(
+                        Message.HeartbeatRspMsg.newBuilder()
+                                .setErrcode(Common.ErrCode.SUCCESS)
+                ).build();
     }
 }
